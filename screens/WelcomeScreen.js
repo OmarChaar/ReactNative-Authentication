7056d4a0ece8a23a057b89451aa4112fb5cc50a9
OmarChaar <oishaar@gmail.com>
2022-05-30 19:20:56 -0300

    checking authentication using tokens

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,30 @@
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+import { useEffect, useState, useContext } from "react";
 
+import { AuthContext } from '../store/auth-context';
 function WelcomeScreen() {
+
+  const [fetchedMessage, setFetchedMessage] = useState('');
+
+
+  const authCtx = useContext(AuthContext);
+  const token = authCtx.token;
+
+  useEffect(() => {
+    axios.get(
+      'https://authenticator-3b5a8-default-rtdb.firebaseio.com/message.json?auth=' + token // send the token to prove authentication
+    ).then((response) => {
+      setFetchedMessage(response.data);
+    })
+  }, [token]);
+
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
